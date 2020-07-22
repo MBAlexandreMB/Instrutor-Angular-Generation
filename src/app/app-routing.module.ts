15f4d130e6908f02a9b0638b1c8d00455e2b99e0
Alexandre M. Bonomi <alexandrembonomi@gmail.com>
2020-07-22 16:11:17 -0300

    navbar and router outlet setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+import { ParticipantesComponent } from './components/participantes/participantes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'participantes', component: ParticipantesComponent },
+  { path: '**', redirectTo:'/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParticipantesComponent } from './components/participantes/participantes.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParticipantesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
