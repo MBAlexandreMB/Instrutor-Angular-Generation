f3466ff5b8fd086cdcd284cf3152659f26d27dd1
Alexandre M. Bonomi <alexandrembonomi@gmail.com>
2020-07-23 01:02:18 -0300

    Listando participantes de uma api fake

--- src/app/components/participantes/participantes.component.ts
+++ src/app/components/participantes/participantes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-participantes',
@@ -6,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./participantes.component.css']
 })
 export class ParticipantesComponent implements OnInit {
+  participantes: {
+    id: number,
+    nome: String,
+    email: String,
+    observacoes: String,
+    turma: String,
+  }[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    http.get<any>(`${environment.api_uri}/participantes`)
+    .subscribe(result => {
+      this.participantes = result;
+    });
+   }
 
   ngOnInit() {
   }
